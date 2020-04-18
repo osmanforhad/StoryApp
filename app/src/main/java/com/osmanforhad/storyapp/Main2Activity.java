b1c8aa13fdb2effe1d5b9d672fd0dc91b096ba9d
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-18 23:49:32 +0600

    modifi second activit java file as i need

--- app/src/main/java/com/osmanforhad/storyapp/Main2Activity.java
+++ app/src/main/java/com/osmanforhad/storyapp/Main2Activity.java
@@ -3,12 +3,27 @@ package com.osmanforhad.storyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    /* declaring variable as my xml ui**/
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
-}
+
+        /* initial the xml ui **/
+        textView = findViewById(R.id.txt);
+
+        /* receive data which was send from 1st Activity **/
+        String dStory = getIntent().getStringExtra("story");
+
+        /* show the data which received from 1st activity **/
+        textView.setText(dStory);
+
+    }//end of the onCreate method
+
+}//end of the class
