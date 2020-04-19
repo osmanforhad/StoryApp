c4f7403b2b9ab65fc5f2451a78b6b70e02a7f7b4
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-20 00:15:08 +0600

    making share button dynamic

--- app/src/main/java/com/osmanforhad/storyapp/Main2Activity.java
+++ app/src/main/java/com/osmanforhad/storyapp/Main2Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class Main2Activity extends AppCompatActivity {
     /* declaring variable as my xml ui**/
     TextView textView;
     Button button;
+    Button share_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class Main2Activity extends AppCompatActivity {
         /* initial the xml ui **/
         textView = findViewById(R.id.txt);
         button = findViewById(R.id.copy_btn);
+        share_btn = findViewById(R.id.share_btn);
 
         /* receive data which was send from 1st Activity **/
         final String dStory = getIntent().getStringExtra("story");
@@ -32,11 +35,11 @@ public class Main2Activity extends AppCompatActivity {
         /* show the data which received from 1st activity **/
         textView.setText(dStory);
 
-        /* creating a OnClickListener for making this button dynamic or clickable**/
+        /* creating a OnClickListener for making copy button dynamic or clickable**/
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /* for copying data **/
+                /* code for copying data **/
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 /* use dStory variable means the specific position form string array**/
                 ClipData clip = ClipData.newPlainText("Text", dStory);
@@ -45,9 +48,31 @@ public class Main2Activity extends AppCompatActivity {
                 /* display message to ensure the data is copy successfully **/
                 Toast.makeText(Main2Activity.this,"copied", Toast.LENGTH_SHORT).show();
 
-            }//end of the OnClickListener method
+            }//end of the onClick method
 
         });//end of the setOnClickListener
+
+        /* creating a OnClickListener for making share button dynamic or clickable**/
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* code for share data **/
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                /* use dStory variable means the specific position form string array**/
+                intent.putExtra(Intent.EXTRA_TEXT, dStory);
+
+                /* type of data which we want to share **/
+                intent.setType("text/pain");
+                /* choose the platform for share the text **/
+                intent = Intent.createChooser(intent, "Share by");
+                /* time to share data **/
+                startActivity(intent);
+
+            }//end of the onClick method
+
+        });//end of the OnClickListener
 
     }//end of the onCreate method
 
